47f673f68ee6e53561e61623c360972aeef2342a
dgunarathna <dhanushkalakshan18@gmail.com>
2026-04-23 19:54:33 +0530

    Make landing navbar sticky on scroll

--- src/main/resources/static/script/seller.js
+++ src/main/resources/static/script/seller.js
@@ -1,8 +1,18 @@
 var cursor = document.querySelector('.cursor');
+var landingNavbar = document.querySelector('.landing-navbar');
 
-document.addEventListener('mousemove', function(e){
-  var x = e.clientX;
-  var y = e.clientY;
-  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
-});
+if (cursor) {
+  document.addEventListener('mousemove', function(e) {
+    cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
+  });
+}
+
+if (landingNavbar) {
+  var toggleNavbarOnScroll = function() {
+    landingNavbar.classList.toggle('is-scrolled', window.scrollY > 24);
+  };
+
+  toggleNavbarOnScroll();
+  window.addEventListener('scroll', toggleNavbarOnScroll, { passive: true });
+}
 
